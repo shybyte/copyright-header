715709cc74ad42f13fb70a2c6eb29bfa9b42148b
shybyte <ein@volloeko.de>
2018-07-27 13:10:29 +0200

    Initial version of cli script

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,44 @@
 #!/usr/bin/env node
 
-// tslint:disable-next-line no-console
-console.log('Hossa!!');
+import child_process from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+const COPYRIGHT_OWNER = 'Copyright Owner';
+const CREATIVE_FILE_EXTENSIONS: ReadonlyArray<string> = ['ts', 'js'];
+const COPYRIGHT_HEADER_REGEXP = /^\/\*[\s\S]*?Copyright[\s\S]*?\*\//;
+const COPYRIGHT_TEMPLATE = `/* Copyright (c) ${COPYRIGHT_OWNER} */`;
+
+const FIND_YEARS_REGEXP = /\b20\d{2}\b/g;
+
+const gitFiles = child_process
+  .execSync('git ls-files', { encoding: 'utf8' })
+  .split('\n');
+
+const creativeFiles = gitFiles.filter(
+  filename =>
+    filename.startsWith('test-data') &&
+    CREATIVE_FILE_EXTENSIONS.includes(path.extname(filename).slice(1))
+);
+
+function updateCopyrightHeader(fileContent: string): string {
+  const headMatch = fileContent.match(COPYRIGHT_HEADER_REGEXP);
+  if (headMatch) {
+    const header = headMatch[0];
+    const copyrightYears = header.match(FIND_YEARS_REGEXP);
+    console.log(header, copyrightYears);
+    return fileContent.replace(COPYRIGHT_HEADER_REGEXP, COPYRIGHT_TEMPLATE);
+  } else {
+    return COPYRIGHT_TEMPLATE + '\n\n' + fileContent;
+  }
+}
+
+for (const fileName of creativeFiles) {
+  const fileContent = fs.readFileSync(fileName, 'utf8');
+  console.log(`Checking ${fileName} ...`);
+  const newFileContent = updateCopyrightHeader(fileContent);
+  if (newFileContent !== fileContent) {
+    console.log(`Update copyright header in  ${fileName}`);
+    fs.writeFileSync(fileName, newFileContent);
+  }
+}
