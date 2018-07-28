adaad7f7060994cd702edab08dae6a4010ddcbba
shybyte <ein@volloeko.de>
2018-07-28 11:56:53 +0200

    Used questionable marco in test

--- src/lib/copyright-header.spec.ts
+++ src/lib/copyright-header.spec.ts
@@ -1,12 +1,32 @@
-import { test } from 'ava';
+// tslint:disable:no-expression-statement no-object-mutation
+
+import { Macro, test } from 'ava';
 import { collectFiles } from './copyright-header';
 
-test('collectFiles filters by include array', t => {
-  t.deepEqual(collectFiles(['test-data']), [
-    'test-data/file-javascript-with-header-start-year-to-year.js',
-    'test-data/file-javascript-with-header-start-year.js',
-    'test-data/file-javascript.js',
-    'test-data/file-typescript.ts'
-  ]);
-  t.deepEqual(collectFiles(['test-data-not-exist']), []);
+const collectFilesTest: Macro = (
+  t,
+  include: ReadonlyArray<string>,
+  expected: ReadonlyArray<string>
+) => {
+  t.deepEqual(collectFiles(include), expected);
+};
+
+collectFilesTest.title = (
+  providedTitle: string,
+  include: ReadonlyArray<string>,
+  expected: ReadonlyArray<string>
+) => `collectFiles - ${providedTitle}: ${include} => ${expected}`;
+
+const ALL_TEST_DATA_FILES: ReadonlyArray<string> = [
+  'test-data/file-javascript-with-header-start-year-to-year.js',
+  'test-data/file-javascript-with-header-start-year.js',
+  'test-data/file-javascript.js',
+  'test-data/file-typescript.ts'
+];
+test('matching files', collectFilesTest, ['test-data'], ALL_TEST_DATA_FILES);
+
+test('no matching files', collectFilesTest, ['test-data-not-exist'], []);
+
+test('collectFiles - no include filter', t => {
+  t.true(collectFiles([]).length > 10);
 });
