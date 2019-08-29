981492408f0f8b73d4f426e6e8d4db217c0c22ac
Mark Terrel <mark@unbounded.systems>
2019-08-29 09:59:33 -0600

    fix: Insert header after hashbang line Also allow whitespece before header

--- src/lib/copyright-header.spec.ts
+++ src/lib/copyright-header.spec.ts
@@ -3,9 +3,11 @@
 // tslint:disable:no-expression-statement no-object-mutation
 
 import test, { ExecutionContext } from 'ava';
-import { testExports } from './copyright-header';
+import { testExports, ValidatedOptions } from './copyright-header';
+import { TEMPLATES } from './templates';
+import { FileInfo } from './types';
 
-const { collectFiles, useTodayAsYearDefault } = testExports;
+const { collectFiles, updateCopyrightHeader, useTodayAsYearDefault } = testExports;
 
 const collectFilesTest = (
   t: ExecutionContext<unknown>,
@@ -51,4 +53,38 @@ test('useTodayAsYearDefault', t => {
     createdYear: thisYear,
     updatedYear: thisYear
   });
+});
+
+const testOpts: ValidatedOptions = {
+  copyrightHolder: 'Test User, Inc.',
+  fix: true,
+  include: ['test/file.ts'],
+  exclude: [],
+  template: TEMPLATES.minimal
+};
+
+const testFileInfo: FileInfo = {
+  filename: 'test/file.ts',
+  createdYear: 2002,
+  updatedYear: 2017
+};
+
+test('hashbang', t => {
+  const origFile = ['#!/bin/sh -some -options', 'File content', 'is here', ''].join('\n');
+  const expected = [
+    '#!/bin/sh -some -options',
+    '',
+    '/* Copyright (c) 2002-2017 Test User, Inc. */',
+    '',
+    'File content',
+    'is here',
+    ''
+  ].join('\n');
+
+  let updated = updateCopyrightHeader(testOpts, testFileInfo, origFile);
+  t.is(updated, expected);
+
+  // Run a second time to ensure idempotence
+  updated = updateCopyrightHeader(testOpts, testFileInfo, updated);
+  t.is(updated, expected);
 });
